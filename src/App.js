739f75eea999db95d58743ec0a17df90eb3cc9c2
Dave Puchyr <dave@slim.avaritia.com>
2020-09-14 20:30:38 +0200

    Add modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,104 @@
 import React from "react";
+import * as Swap from "swap-sdk";
 
 
 function App() {
+   // configuration for buying a starname
+   const configStarname = {
+      payment_type: "iov",
+      email: "starname@example.com",
+      email_editable: true,
+   };
+   // configuration for buying IOV tokens
+   const configTokens = {
+      payment_type: "merchant",
+      email: "tokens@example.com",
+      email_editable: true,
+      currency: "iov",
+      amount: 100,
+      amount_editable: true,
+   };
+
+   // modal buttons
+   const modalButtonStarname = new Swap.Widget( {
+      type: "modal",
+      buttonId: "button-starname",
+      mode: "production",
+      config: configStarname,
+   } );
+   const modalButtonTokens = new Swap.Widget( {
+      type: "modal",
+      buttonId: "button-tokens",
+      mode: "production",
+      config: configTokens,
+   } );
+
+   // modal spans
+   const modalSpanStarname = new Swap.Widget( {
+      type: "modal",
+      buttonId: "span-starname", // use buttonId despite applyling to a span
+      mode: "production",
+      config: configStarname,
+   } );
+   const modalSpanTokens = new Swap.Widget( {
+      type: "modal",
+      buttonId: "span-tokens", // use buttonId despite applyling to a span
+      mode: "production",
+      config: configTokens,
+   } );
+
+   // initializations
+   [
+      modalButtonStarname,
+      modalButtonTokens,
+      modalSpanStarname,
+      modalSpanTokens,
+   ].forEach( savitar => savitar.init() );
+
+   // styles
+   const margin = {
+      margin: "10px",
+   };
+   const styleDiv = {
+      display: "flex",
+      justifyContent: "space-between",
+      width: "100%",
+   };
+   const styleSpan = {
+      border: "1px solid black",
+      backgroundColor: "black",
+      color: "white",
+   };
+
    return (
-      <div className="App">
-         App
+      <div style={margin}>
+         <p>
+            Check out Savitar's <a href="https://github.com/savitar-exchange/swap-sdk" target="_blank" rel="noopener noreferrer">swap-sdk</a> for more configuration options and event listener documentation.
+         </p>
+
+         <fieldset style={margin}>
+            <legend>Modal Widget</legend>
+            <div style={styleDiv}>
+               <button id="button-starname">
+                  Buy A Starname
+               </button>
+
+               <button id="button-tokens">
+                  Buy IOV Tokens
+               </button>
+
+               <span id="span-starname" style={styleSpan}>
+                  Buy A Starname
+               </span>
+
+               <span id="span-tokens" style={styleSpan}>
+                  Buy IOV Tokens
+               </span>
+            </div>
+         </fieldset>
+         <fieldset style={margin}>
+            <legend>Embedded Widget</legend>
+         </fieldset>
       </div>
    );
 }
