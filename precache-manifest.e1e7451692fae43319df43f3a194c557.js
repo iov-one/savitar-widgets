b6e80747863ce5038c8f80a3d2ed4f121dfc8038
Dave Puchyr <dave@slim.avaritia.com>
2020-09-15 09:53:31 +0200

    Updates

---
+++ precache-manifest.e1e7451692fae43319df43f3a194c557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec50f8cb1b3192022d0c4e1f72ef73b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1156274da7f9c022fa56",
+    "url": "/static/css/main.82814904.chunk.css"
+  },
+  {
+    "revision": "f3e8f2e4e48a25364d5f",
+    "url": "/static/js/2.51dfc759.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.51dfc759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1156274da7f9c022fa56",
+    "url": "/static/js/main.c56e725e.chunk.js"
+  },
+  {
+    "revision": "8bee202b468b8211b45c",
+    "url": "/static/js/runtime-main.50fc7542.js"
+  }
+]);
