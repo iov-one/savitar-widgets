6d8507fe4103e1ae3d1e6d84e95d5a0463235c92
Dave Puchyr <dave@slim.avaritia.com>
2020-09-15 09:17:09 +0200

    Convert App to a class

--- src/App.js
+++ src/App.js
@@ -2,133 +2,137 @@ import React from "react";
 import * as Swap from "swap-sdk";
 
 
-function App() {
-   // configuration for buying a starname
-   const configStarname = {
-      payment_type: "iov",
-      email: "starname@example.com",
-      email_editable: true,
-   };
-   // configuration for buying IOV tokens
-   const configTokens = {
-      payment_type: "merchant",
-      email: "tokens@example.com",
-      email_editable: true,
-      currency: "iov",
-      amount: 100,
-      amount_editable: true,
-   };
+// configuration for buying a starname
+const configStarname = {
+   payment_type: "iov",
+   email: "starname@example.com",
+   email_editable: true,
+};
+// configuration for buying IOV tokens
+const configTokens = {
+   payment_type: "merchant",
+   email: "tokens@example.com",
+   email_editable: true,
+   currency: "iov",
+   amount: 100,
+   amount_editable: true,
+};
 
-   // embedded
-   const embeddedStarname = new Swap.Widget( {
-      type: "embed",
-      embedContainerId: "div-starname",
-      mode: "production",
-      config: configStarname,
-   } );
-   const embeddedTokens = new Swap.Widget( {
-      type: "embed",
-      embedContainerId: "div-tokens",
-      mode: "production",
-      config: configTokens,
-   } );
+// embedded widget
+const embeddedStarname = new Swap.Widget( {
+   type: "embed",
+   embedContainerId: "div-starname",
+   mode: "production",
+   config: configStarname,
+} );
+const embeddedTokens = new Swap.Widget( {
+   type: "embed",
+   embedContainerId: "div-tokens",
+   mode: "production",
+   config: configTokens,
+} );
 
-   // modal buttons
-   const modalButtonStarname = new Swap.Widget( {
-      type: "modal",
-      buttonId: "button-starname",
-      mode: "production",
-      config: configStarname,
-   } );
-   const modalButtonTokens = new Swap.Widget( {
-      type: "modal",
-      buttonId: "button-tokens",
-      mode: "production",
-      config: configTokens,
-   } );
+// modal buttons
+const modalButtonStarname = new Swap.Widget( {
+   type: "modal",
+   buttonId: "button-starname",
+   mode: "production",
+   config: configStarname,
+} );
+const modalButtonTokens = new Swap.Widget( {
+   type: "modal",
+   buttonId: "button-tokens",
+   mode: "production",
+   config: configTokens,
+} );
 
-   // modal spans
-   const modalSpanStarname = new Swap.Widget( {
-      type: "modal",
-      buttonId: "span-starname", // use buttonId despite applyling to a span
-      mode: "production",
-      config: configStarname,
-   } );
-   const modalSpanTokens = new Swap.Widget( {
-      type: "modal",
-      buttonId: "span-tokens", // use buttonId despite applyling to a span
-      mode: "production",
-      config: configTokens,
-   } );
+// modal spans
+const modalSpanStarname = new Swap.Widget( {
+   type: "modal",
+   buttonId: "span-starname", // use buttonId despite applyling to a span
+   mode: "production",
+   config: configStarname,
+} );
+const modalSpanTokens = new Swap.Widget( {
+   type: "modal",
+   buttonId: "span-tokens", // use buttonId despite applyling to a span
+   mode: "production",
+   config: configTokens,
+} );
 
-   // initializations
-   [
-      //embeddedStarname,
-      //embeddedTokens,
-      modalButtonStarname,
-      modalButtonTokens,
-      modalSpanStarname,
-      modalSpanTokens,
-   ].forEach( savitar => savitar.init() );
+// styles
+const margin = {
+   margin: "10px",
+};
+const styleDiv = {
+   display: "flex",
+   justifyContent: "space-between",
+   width: "100%",
+};
+const styleEmbedded = {
+   border: "1px solid red",
+   height: "67%",
+   width: "49%",
+};
+const styleSpan = {
+   border: "1px solid black",
+   backgroundColor: "black",
+   color: "white",
+};
 
-   // styles
-   const margin = {
-      margin: "10px",
-   };
-   const styleDiv = {
-      display: "flex",
-      justifyContent: "space-between",
-      width: "100%",
-   };
-   const styleEmbedded = {
-      border: "1px solid red",
-      height: "67%",
-      width: "49%",
-   };
-   const styleSpan = {
-      border: "1px solid black",
-      backgroundColor: "black",
-      color: "white",
-   };
 
-   return (
-      <div style={margin}>
-         <p>
-            Check out Savitar's <a href="https://github.com/savitar-exchange/swap-sdk" target="_blank" rel="noopener noreferrer">swap-sdk</a> for more configuration options and event listener documentation.
-         </p>
+class App extends React.Component {
+   componentDidMount() {
+      // initializations
+      [
+         embeddedStarname,
+         embeddedTokens,
+         modalButtonStarname,
+         modalButtonTokens,
+         modalSpanStarname,
+         modalSpanTokens,
+      ].forEach( savitar => savitar.init() );
+   }
 
-         <fieldset style={margin}>
-            <legend>Modal Widget</legend>
-            <div style={styleDiv}>
-               <button id="button-starname">
-                  Buy A Starname
-               </button>
 
-               <button id="button-tokens">
-                  Buy IOV Tokens
-               </button>
+   render() {
+      return (
+         <div style={margin}>
+            <p>
+               Check out Savitar's <a href="https://github.com/savitar-exchange/swap-sdk" target="_blank" rel="noopener noreferrer">swap-sdk</a> for more configuration options and event listener documentation.
+            </p>
 
-               <span id="span-starname" style={styleSpan}>
-                  Buy A Starname
-               </span>
+            <fieldset style={margin}>
+               <legend>Modal Widget</legend>
+               <div style={styleDiv}>
+                  <button id="button-starname">
+                     Buy A Starname
+                  </button>
 
-               <span id="span-tokens" style={styleSpan}>
-                  Buy IOV Tokens
-               </span>
-            </div>
-         </fieldset>
-         {false &&
-         <fieldset style={margin}>
-            <legend>Embedded Widget</legend>
-            <div style={styleDiv}>
-               <div id="div-starname" style={styleEmbedded} />
+                  <button id="button-tokens">
+                     Buy IOV Tokens
+                  </button>
 
-               <div id="div-tokens" style={styleEmbedded} />
-            </div>
-         </fieldset>
-         }
-      </div>
-   );
+                  <span id="span-starname" style={styleSpan}>
+                     Buy A Starname
+                  </span>
+
+                  <span id="span-tokens" style={styleSpan}>
+                     Buy IOV Tokens
+                  </span>
+               </div>
+            </fieldset>
+            <fieldset style={margin}>
+               <legend>Embedded Widget</legend>
+               <div style={styleDiv}>
+                  <div id="div-starname" style={styleEmbedded} />
+
+                  <div id="div-tokens" style={styleEmbedded} />
+               </div>
+            </fieldset>
+         </div>
+      );
+   }
 }
 
 
